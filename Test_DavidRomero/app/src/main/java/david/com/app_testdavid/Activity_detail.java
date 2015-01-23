9e96f816a1e7ba4b39465e344842091f83f42699
David Romero <david.rm860@live.com>
2015-01-23 13:27:53 -0600

    detail_design

--- Test_DavidRomero/app/src/main/java/david/com/app_testdavid/Activity_detail.java
+++ Test_DavidRomero/app/src/main/java/david/com/app_testdavid/Activity_detail.java
@@ -2,6 +2,7 @@ package david.com.app_testdavid;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
 
 import david.com.app_testdavid.utils.Utils;
 
@@ -11,10 +12,30 @@ import david.com.app_testdavid.utils.Utils;
 public class Activity_detail extends ActionBarActivity{
 
     String id, mag, place, alert, status, tsunami, code, magType, type, title;
+
+    TextView tex_title,tex_id,tex_place,tex_mag,tex_alert,tex_tsu;
+
+    TextView tex_code, tex_magType, tex_type;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        tex_title=(TextView)findViewById(R.id.tex_title);
+        tex_id=(TextView)findViewById(R.id.tex_id);
+        tex_place=(TextView)findViewById(R.id.tex_place);
+        tex_mag=(TextView)findViewById(R.id.tex_mag);
+        tex_alert=(TextView)findViewById(R.id.tex_alert);
+        tex_tsu=(TextView)findViewById(R.id.tex_tsu);
+
+        tex_code=(TextView)findViewById(R.id.tex_code);
+        tex_magType=(TextView)findViewById(R.id.tex_magType);
+        tex_type=(TextView)findViewById(R.id.tex_type);
+
+
+
+
+
 
 
         Bundle bundle=getIntent().getExtras();
@@ -31,7 +52,18 @@ public class Activity_detail extends ActionBarActivity{
             type=bundle.getString("type");
             title=bundle.getString("title");
 
-            Utils.createAlert(Activity_detail.this, id+"\n"+ mag+ "\n"+place+ "\n"+alert+"\n"+ status+ "\n"+ tsunami+ "\n"+ code+ "\n"+ magType+"\n"+  type+"\n"+  title);
+
+            tex_title.setText(title);
+            tex_id.setText("ID:\n"+id);
+            tex_place.setText("Place :\n"+place);
+            tex_mag.setText("Magnitude :\n"+mag);
+            tex_alert.setText("Alert :"+alert);
+            tex_tsu.setText("Tsunami :\n"+tsunami);
+
+            tex_code.setText("Code :\n"+code);
+            tex_magType.setText("Magnitude Type :\n"+magType);
+            tex_type.setText("Type :\n"+type);
+
         }
 
 
